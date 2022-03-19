eafd230e240d335e7dba06d4dd98403584fed3d9
Aloizio Macedo <aloiziomacedo@gmail.com>
2022-03-19 17:01:29 -0300

    Make diff to be None when arg has no attributes

--- side_effect/decorators.py
+++ side_effect/decorators.py
@@ -1,10 +1,12 @@
 from copy import deepcopy
-from typing import Any, Callable, Dict
+from typing import Any, Callable, Dict, Union
 
 
-def _get_top_level_diff(new: Any, old: Any) -> Dict[str, Any]:
+def _get_top_level_diff(new: Any, old: Any) -> Union[Dict[str, Any], None]:
     if not isinstance(old, type(new)):
         raise ValueError("Trying to check diff of incompatible types.")
+    if not hasattr(old, "__dict__"):
+        return None
     new_attrs = vars(new)
     old_attrs = vars(old)
     diff: Dict[str, Any] = {}
@@ -18,11 +20,13 @@ def se_print(func: Callable) -> Callable:
     """Transforms the function to print its side-effects on the arguments."""
     def g(*args):
         previous_states = deepcopy(args)
-        result = func(*args)
         print(f"Call of {func.__name__} on args "
               f"{tuple((str(arg) for arg in args))}:")
+        result = func(*args)
         for arg, previous_state in zip(args, previous_states):
             diff = _get_top_level_diff(arg, previous_state)
+            if diff is None:
+                continue
             for key, attr in diff.items():
                 print(f"    Attribute {key} of {str(arg)} changed"
                       f" from {str(getattr(previous_state, key))}"
