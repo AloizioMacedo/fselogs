ad7646a456081674b03dd94ada92b1e0567ba055
Aloizio Macedo <aloiziomacedo@gmail.com>
2022-03-20 00:52:41 -0300

    Implement intended behavior in se_print
    se_print is intended to be a shallow verification based on attributes
    of the arguments. In the way it was implemented, it was not cognisant
    of the fact that multiple classes are not defined with a conscientious
    __eq__ method.

--- side_effect/decorators.py
+++ side_effect/decorators.py
@@ -2,17 +2,20 @@ from copy import deepcopy
 from typing import Any, Callable, Dict, Union
 
 
-def _get_top_level_diff(new: Any, old: Any) -> Union[Dict[str, Any], None]:
+def _has_eq_defined(obj: Any) -> bool:
+    return hasattr(obj.__eq__, "__func__")
+
+
+def _get_top_level_diff(new: Any, old: Any) -> Dict[str, Any]:
     if not isinstance(old, type(new)):
         raise ValueError("Trying to check diff of incompatible types.")
-    if not hasattr(old, "__dict__"):
-        return None
     new_attrs = vars(new)
     old_attrs = vars(old)
     diff: Dict[str, Any] = {}
     for key, attr in new_attrs.items():
-        if key not in old_attrs or attr != old_attrs[key]:
-            diff[key] = deepcopy(attr)
+        if ((key not in old_attrs or attr != old_attrs[key])):
+            if not hasattr(attr, "__dict__") or _has_eq_defined(attr):
+                diff[key] = deepcopy(attr)
     return diff
 
 
@@ -23,14 +26,24 @@ def se_print(func: Callable) -> Callable:
         print(f"Call of {func.__name__} on args "
               f"{tuple((str(arg) for arg in args))}:")
         result = func(*args)
+        found = False
         for arg, previous_state in zip(args, previous_states):
-            diff = _get_top_level_diff(arg, previous_state)
-            if diff is None:
-                continue
-            for key, attr in diff.items():
-                print(f"    Attribute {key} of {str(arg)} changed"
-                      f" from {str(getattr(previous_state, key))}"
-                      f" to {str(attr)}.")
+            if hasattr(arg, "__dict__"):
+                diff = _get_top_level_diff(arg, previous_state)
+                if not diff:
+                    continue
+                for key, attr in diff.items():
+                    print(f"    Attribute {key} of {str(arg)} changed"
+                          f" from {str(getattr(previous_state, key))}"
+                          f" to {str(attr)}.")
+                    found = True
+            else:
+                if arg != previous_state:
+                    print(f"    Argument {str(arg)} changed"
+                          f" from {str(previous_state)}"
+                          f" to {str(arg)}.")
+        if not found:
+            print("    No side effects found.")
         return result
     return g
 
